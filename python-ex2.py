b5e26f32a8bee3e8c00c599ccd707f31a964a953
Vladyslav Filatov <vladyslavfilatov@Air-Vladyslav.Home>
2022-06-19 18:02:12 +0100

    ex2and3

---
+++ python-ex2.py
@@ -0,0 +1,6 @@
+print("Introduza o raio")
+r = int(input())
+print("Introduza a altura")
+h = int(input())
+res = 3.14 * 4
+print(f"O resultado {res}")
---
+++ python-ex3.py
@@ -0,0 +1,8 @@
+print("Introduza o tempo de viagem")
+t = float(input())
+print("Introduza a velocidade km/h");
+v = float(input())
+dist = t * v
+l = 8 * dist / 100
+print(f"O total de litros consumidos é:{l}")
+input()
